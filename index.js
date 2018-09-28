6ee4e7108c88da4e4ea0944532052e2f983f1c7b
Ian Nastajus <ian.nastajus@gmail.com>
2018-09-28 16:23:03 -0400

    updated with rxjs code/refs

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var arithmetic = require('arithmetic');
-arithmetic.add(2, 4);
+import { Observable, Subject, ReplaySubject, from, of, range } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
 
+range(1, 200)
+  .pipe(filter(x => x % 2 === 1), map(x => x + x))
+  .subscribe(x => console.log(x));
